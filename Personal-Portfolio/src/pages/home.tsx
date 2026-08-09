3b63b849750d6bc2f6aa20c16bfbd97f19305ae5
Dinil_Denny <dinildenny99@gmail.com>
2026-08-09 13:39:15 +0530

    Implemented folder sturcture,theme and basic code

---
+++ Personal-Portfolio/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import Hero from "../sections/hero";
+
+const Home = () => {
+  return (
+    <>
+        <Hero/>
+    </>
+  )
+}
+
+export default Home;
---
+++ Personal-Portfolio/src/sections/hero.tsx
@@ -0,0 +1,10 @@
+
+const Hero = () => {
+  return (
+    <div>
+       <h1 className="text-3xl font-bold text-primary">This is home page</h1>
+    </div>
+  )
+}
+
+export default Hero;
